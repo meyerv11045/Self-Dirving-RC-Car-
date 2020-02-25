fcd62fb06c71f086be6a201e60913cb9f50adb73
meyerv11045 <33674604+meyerv11045@users.noreply.github.com>
2020-02-25 13:57:31 -0500

    Update and rename build_opencv.sh to opencv_dependencies.sh

---
+++ opencv_dependencies.sh
@@ -0,0 +1,6 @@
+sudo apt install libgtk-3-dev libcanberra-gtk3-dev #For use of OpenCV GUI version
+sudo apt install libtiff-dev zlib1g-dev
+sudo apt install libjpeg-dev libpng-dev
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt install python-numpy python3-numpy
